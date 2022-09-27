05df193bb6131ccdff33a9295f30f06c6594f924
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-27 05:47:20 +0000

    Refactor(scripts): config variable names
    - Rename default kubernetes watch resources variable to
      `DEFAULT_KUBECTL_WATCH_RESOURCES`. This is done so that every
      default value can have the prefix `DEFAULT_`.
    - Move `TF_VARS_FILE_DISABLED_PHRASE` to to `.repo.config` file.
      Previously this value was expected to be provided in `.env` but
      it makes much more sense that this value should come from
      `.repo.config`.

--- scripts/kubectl-watch.sh
+++ scripts/kubectl-watch.sh
@@ -4,7 +4,7 @@ source scripts/config.sh || exit 1
 check_env
 check_repo_config
 
-resources=${1:-$KUBECTL_WATCH_DEFAULT_RESOURCES}
+resources=${1:-$DEFAULT_KUBECTL_WATCH_RESOURCES}
 
 echo "watch '\
   for resource in $resources; do \
